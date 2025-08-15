2b9810e5fc0f2cfe78c2a3cf38422c2cfc09f710
GLITCH <67582658+arcofchanu@users.noreply.github.com>
2025-08-16 03:38:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ui1/',
   resolve: {
     dedupe: ['react', 'react-dom'],
     alias: { fs: 'fs' }
